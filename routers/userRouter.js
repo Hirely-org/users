01058a68a8d67e96e181d00dcd736f4084ef857a
Škrbić Veljko <494716@student.fontys.nl>
2025-01-15 01:05:14 +0100

    asdsa

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -2,41 +2,56 @@ const router = require('express').Router();
 const RabbitMQService = require('../rabbitMQService');
 const db = require('../models');
 const jwt = require('jsonwebtoken');
+const jwksClient = require('jwks-rsa');
 
 router.get('/', async (req, res) => {
     const users = "Hello World";
     return res.json(users);
 });
 
-// add a new route that get job by id
-router.get('/:id', async (req, res) => {
-    let user;
-    try{
-        user = await db.Users.findByPk(req.params.id);
-    } catch(error){
-        console.error('Error getting job:', error);
-        return res.status(500).send('Error getting job');
-    }
-    console.log('Job:', user);
-    return res.json(user);
+// // add a new route that get job by id
+// router.get('/:id', async (req, res) => {
+//     let user;
+//     try{
+//         user = await db.Users.findByPk(req.params.id);
+//     } catch(error){
+//         console.error('Error getting job:', error);
+//         return res.status(500).send('Error getting job');
+//     }
+//     console.log('Job:', user);
+//     return res.json(user);
+// });
+
+const client = jwksClient({
+  jwksUri: 'https://hirely-dev.eu.auth0.com/.well-known/jwks.json', // Replace with your Auth0 domain
 });
 
+// Function to get the signing key
+const getKey = (header, callback) => {
+  client.getSigningKey(header.kid, (err, key) => {
+    if (err) {
+      return callback(err, null);
+    }
+    const signingKey = key.publicKey || key.rsaPublicKey;
+    callback(null, signingKey);
+  });
+};
+
 router.post('/auth', (req, res) => {
-  // Extract token without the 'Bearer ' prefix
-  const token = req.headers['authorization']?.split(' ')[1];  // Split to get the token part after "Bearer"
-  
+  const token = req.headers['authorization']?.split(' ')[1]; // Extract token
+
   if (!token) {
     return res.status(401).json({ message: 'No token provided' });
   }
-
-  // Validate the token
-  jwt.verify(token, 'your-secret-key', (err, decoded) => {
+  console.log(token);
+  // Verify the token using RS256 and the JWKS public key
+  jwt.verify(token, getKey, { algorithms: ['RS256'] }, (err, decoded) => {
     if (err) {
       return res.status(403).json({ message: 'Invalid token' });
     }
 
     // If token is valid, forward user info in response headers
-    res.setHeader('X-Forwarded-User', decoded.user);
+    // res.setHeader('X-Forwarded-User', decoded.user);
     return res.status(200).json({ message: 'Token valid' });
   });
 });

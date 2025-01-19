d7ed25c36b4957336484eecc1731bbd98ee74eba
Škrbić Veljko <494716@student.fontys.nl>
2025-01-19 04:27:15 +0100

    asfasf

--- constants/messageTypes.js
+++ constants/messageTypes.js
@@ -1,10 +1,21 @@
 // constants/messageTypes.js
 const MessageTypes = {
-    // Commands
+    // User service events
     DELETE_USER_START: 'delete_user_start',
     DELETE_USER_SUCCESS: 'delete_user_success',
     DELETE_USER_FAILED: 'delete_user_failed',
-    ROLLBACK_DELETE_USER: 'rollback_delete_user'
+    
+    // Job Application service events
+    JOB_APPLICATIONS_DELETED: 'job_applications_deleted',
+    JOB_APPLICATIONS_DELETION_FAILED: 'job_applications_deletion_failed',
+    
+    // Rollback events
+    ROLLBACK_DELETE_USER: 'rollback_delete_user',
+    ROLLBACK_JOB_APPLICATIONS: 'rollback_job_applications',
+
+    // Final saga status
+    SAGA_COMPLETED: 'saga_completed',
+    SAGA_FAILED: 'saga_failed'
 };
 
 module.exports = MessageTypes;
